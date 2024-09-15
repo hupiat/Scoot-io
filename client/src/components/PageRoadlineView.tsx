7bc50a9acd65c93c011162ac5d470689792f4dcb
Hugo Piat <h.piat@outlook.fr>
2024-09-16 01:24:35 +0200

    added custom marker (scooter)

--- client/src/components/PageRoadlineView.tsx
+++ client/src/components/PageRoadlineView.tsx
@@ -1,17 +1,27 @@
 import {View} from '@ant-design/react-native';
 import React, {useEffect, useState} from 'react';
-import {Dimensions, StyleSheet} from 'react-native';
-import MapView, {Marker, MarkerAnimated} from 'react-native-maps';
+import {Dimensions, ImageURISource, StyleSheet} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
 import * as Location from 'expo-location';
+import MapViewDirections from 'react-native-maps-directions';
+import MarkerScooter from '../assets/marker.png';
+
+const TIMEOUT_DELAY_LOCATION_MS = 500;
 
 export default function PageRoadlineView() {
   const [position, setPosition] = useState<Location.LocationObjectCoords>();
 
   useEffect(() => {
     // Permissions are requested from the login page
-    Location.getCurrentPositionAsync().then(location =>
-      setPosition(location.coords),
-    );
+    let timeoutId: NodeJS.Timeout;
+    const timeout = () => {
+      Location.getCurrentPositionAsync().then(location =>
+        setPosition(location.coords),
+      );
+      timeoutId = setTimeout(timeout, TIMEOUT_DELAY_LOCATION_MS);
+    };
+    timeout();
+    return () => clearTimeout(timeoutId);
   }, [Location, setPosition]);
 
   return (
@@ -23,19 +33,26 @@ export default function PageRoadlineView() {
             ? {
                 latitude: position.latitude,
                 longitude: position.longitude,
-                latitudeDelta: 0,
-                longitudeDelta: 0,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
               }
             : undefined
         }>
         {position && (
-          <MarkerAnimated
+          <Marker
             coordinate={{
               latitude: position.latitude,
               longitude: position.longitude,
             }}
+            image={require('../assets/marker.png')}
+            style={{width: 50, height: 50}}
           />
         )}
+        <MapViewDirections
+          apikey="AIzaSyC5K8oefgx7p-owCOLThF38oSnYhQAPFVI"
+          mode="BICYCLING"
+          optimizeWaypoints
+        />
       </MapView>
     </View>
   );
