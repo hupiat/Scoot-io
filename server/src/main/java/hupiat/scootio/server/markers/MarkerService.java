459b638ad420fa02846023ee984e11994d96d619
Hugo Piat <h.piat@outlook.fr>
2024-09-21 19:12:54 +0200

    added @Transactional for fetching by radius

--- server/src/main/java/hupiat/scootio/server/markers/MarkerService.java
+++ server/src/main/java/hupiat/scootio/server/markers/MarkerService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import hupiat.scootio.server.chargingStations.CharginStationRepository;
 import hupiat.scootio.server.chargingStations.ChargingStationEntity;
+import jakarta.transaction.Transactional;
 
 @Service
 public class MarkerService {
@@ -25,6 +26,7 @@ public class MarkerService {
 		this.charginStationRepository = charginStationRepository;
 	}
 
+	@Transactional
 	public List<MarkerEntity> fetchAllMarkersByRadius(double longitude, double latitude) {
 		List<MarkerEntity> res = new ArrayList<>();
 		List<GeocodeEntity> geometries = geocodeRepository.findGeocodesAroundPosition(longitude, latitude, LOCAL_SEARCH_RADIUS_METERS);
@@ -34,6 +36,7 @@ public class MarkerService {
 		return res;
 	}
 	
+	@Transactional
 	public List<ChargingStationEntity> fetchAllChargingStationsByRadius(double longitude, double latitude) {
 		List<ChargingStationEntity> res = new ArrayList<>();
 		List<GeocodeEntity> geometries = geocodeRepository.findGeocodesAroundPosition(longitude, latitude, LOCAL_SEARCH_RADIUS_METERS);
